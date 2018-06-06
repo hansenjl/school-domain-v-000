0de0699e2ddac82eb852aaa542d9938d4706f98b
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-06-06 13:54:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,28 +1,2 @@
-class School
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end
-  def name
-    @name
-  end
-  def roster
-    @roster
-  end
-  def add_student(student,grade)
-    @roster[grade] = [] if @roster.keys.include?(grade) != true
-    @roster[grade] << student
-  end
-  def grade(grade)
-    @roster[grade]
-  end
-  def sort
-    @roster.each do |grade,students|
-      students.sort!
-    end
-  end
-end
-
-
 
 
